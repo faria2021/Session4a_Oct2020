0a311e6dad753ba4f9419e541ffe35ae4837c4ac
faria2021 <79468356+faria2021@users.noreply.github.com>
2021-07-29 10:24:09 -0500

    Session5a

---
+++ src/main/java/variousConcepts/NewBranch.java
@@ -0,0 +1,19 @@
+package variousConcepts;
+
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+public class NewBranch {
+	public class TestGit1 {
+@BeforeClass
+		public void main(String[] args) {
+			printMe();
+		}
+@Test		
+		public void printMe() {
+			System.out.println("test git");
+		
+		
+		}
+
+}}
